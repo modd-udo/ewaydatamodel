192b4af9ee3b537e269358432bd8614c27c9337e
Chris Seufert <chris@seufert.id.au>
2015-10-23 12:35:48 +1100

    Added fromJson func

--- php/DirectPayment/Payment.php
+++ php/DirectPayment/Payment.php
@@ -59,7 +59,7 @@ class Payment implements JsonSerializable {
   /**
    * The merchant’s reference number for this transaction
    * (optional) max 50 chars
-   * @var
+   * @var string
    */
   public $invoiceReference;
 
@@ -80,4 +80,25 @@ class Payment implements JsonSerializable {
         $o[ucfirst($key)] = $value;
     return $o;
   }
+
+  /**
+   * @param string|array $json
+   * @return Payment
+   * @throws \Exception
+   */
+  static function fromJson($json) {
+    if(is_object($json))
+      $json = (array)$json;
+    elseif(!is_array($json))
+      $json = json_decode($json, JSON_OBJECT_AS_ARRAY);
+    else
+      throw new \Exception("Unable to determine JSON Data type");
+
+    $res = new self();
+    foreach($res as $key => $value)
+      if(isset($json[ucfirst($key)]) && $json[ucfirst($key)])
+        $res->$key = $json[ucfirst($key)];
+    return $res;
+  }
+
 }
